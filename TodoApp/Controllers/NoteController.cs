0103e379bfc11305487767f3708ce26919aa9a45
Claudio Caldarella <claudiocaldr@gmail.com>
2024-04-07 22:11:56 +0200

    feat: aggiunta gestione dei null nel metodo di deserializzazione feat: aggiunto supporto alla paginazione

--- TodoApp/Controllers/NoteController.cs
+++ TodoApp/Controllers/NoteController.cs
@@ -2,6 +2,7 @@
 using DataAccess.Models;
 using DataAccess;
 using TodoApp.Helpers;
+using Microsoft.EntityFrameworkCore;
 
 namespace TodoApp.Controllers
 {
@@ -12,36 +13,50 @@ namespace TodoApp.Controllers
         {
             this._db = applicationDbContext;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int? pageNumber)
         {
-            var data = TempDataExtensions.Get<List<Note>>(TempData, "notes");
-            if (data != null)
+            int pageSize = 1;
+            if (TempData["notes"] != null)
             {
-                return View(data);
+                var data = TempDataExtensions.Get<List<Note>>(TempData, "notes");
+                return View(PaginatedList<Note>.Create(data, pageNumber ?? 1, pageSize));
             }
-            List<Note> notes = this._db.Notes.OrderBy(x => x.Id).ToList();
-            return View(notes);
-            
+
+            if (pageNumber > 0)
+            {
+                var notesList = await PaginatedList<Note>.CreateAsync(this._db.Notes.AsNoTracking(), pageNumber ?? 1, pageSize);
+                if (notesList.Count > 0)
+                {
+                    return View(notesList);
+                }
+                else { return View(await PaginatedList<Note>.CreateAsync(this._db.Notes.AsNoTracking(), (pageNumber != null ? (int)pageNumber - 1 : 1), pageSize)); }
+            }
+            else
+            {
+                return View(await PaginatedList<Note>.CreateAsync(this._db.Notes.AsNoTracking(), 1, pageSize));
+            }
         }
         [HttpPost]
-        public IActionResult SearchByTitle(string name)
+        public async Task<IActionResult> SearchByTitle(string name)
         {
-            List<Note> notes = this._db.Notes.Where(n => n.Title.Contains(name)).ToList();
+            int pageSize = 2;
+            PaginatedList<Note> notes = await PaginatedList<Note>.CreateAsync(this._db.Notes.AsNoTracking().Where(x => x.Title.Contains(name)), 1, pageSize);
             TempDataExtensions.Put(TempData, "notes", notes);
             return RedirectToAction("Index");
         }
 
         [HttpPost]
-        public IActionResult SearchByDescription(string name)
+        public async Task<IActionResult> SearchByDescription(string name)
         {
-            List<Note> notes = this._db.Notes.Where(n => n.Description.Contains(name)).ToList();
+            int pageSize = 2;
+            PaginatedList<Note> notes = await PaginatedList<Note>.CreateAsync(this._db.Notes.AsNoTracking().Where(x => x.Description.Contains(name)), 1, pageSize);
             TempDataExtensions.Put(TempData, "notes", notes);
             return RedirectToAction("Index");
         }
 
         public IActionResult Create()
         {
-                return View("Create");
+            return View("Create");
         }
 
         [HttpPost]
@@ -59,9 +74,11 @@ namespace TodoApp.Controllers
         public IActionResult Edit(int id)
         {
             Note? note = _db.Notes.Find(id);
-            if(note == null) { 
-               return RedirectToAction("Index");
-            } else
+            if (note == null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
             {
                 return View("Edit", note);
             }
@@ -69,9 +86,9 @@ namespace TodoApp.Controllers
         [HttpPost]
         public IActionResult Edit(Note note)
         {
-                _db.Notes.Update(note);
-                _db.SaveChanges();
-                return RedirectToAction("Index");
+            _db.Notes.Update(note);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         public IActionResult Delete(int id)
--- TodoApp/Helpers/TempDataExtensions.cs
+++ TodoApp/Helpers/TempDataExtensions.cs
@@ -10,7 +10,7 @@ namespace TodoApp.Helpers
             tempData[key] = JsonConvert.SerializeObject(value);
         }
 
-        public static T Get<T>(this ITempDataDictionary tempData, string key) where T : class
+        public static T? Get<T>(this ITempDataDictionary tempData, string key) where T : class
         {
             object o;
             tempData.TryGetValue(key, out o);
